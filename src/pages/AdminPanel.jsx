c74065a0250a6d10696e8a70276a0112ceeb03fb
Tabarnam <jon@tabarnam.com>
2025-07-15 19:27:31 -0700

    Create AdminPanel.jsx

---
+++ src/pages/AdminPanel.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function AdminPanel() {
+  return (
+    <div className="p-6 space-y-6">
+      <h1 className="text-3xl font-bold">Admin Panel</h1>
+      <ul className="space-y-4">
+        <li>
+          <Link to="/admin/xai-bulk-import" className="text-blue-600 underline">
+            xAI Bulk Import Tool
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
