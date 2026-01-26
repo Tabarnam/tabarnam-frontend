f50dcb17fb8a004a3ce8d44bf6397abe63cab4e3
duh@tabarnam.com <jon@tabarnam.com>
2026-01-25 20:35:50 -0800

    fix resume worker registration and add queue trigger

--- api/import/resume-worker/index.js
+++ api/import/resume-worker/index.js
@@ -1,6 +1,13 @@
 const { app } = require("@azure/functions");
 const { resumeWorkerHandler } = require("./handler");
 
+/**
+ * HTTP-triggered entrypoint for the resume worker.
+ * IMPORTANT:
+ * - This file must ONLY register the function.
+ * - NO logic, NO side effects, NO imports that execute work.
+ */
+
 app.http("import-resume-worker", {
   route: "import/resume-worker",
   methods: ["POST", "OPTIONS"],
